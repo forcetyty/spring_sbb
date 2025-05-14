107e64d2c8bbb655790aca59e7b8c17d39ee0ac4
taeyoung <forcetyty@gmail.com>
2025-05-15 02:13:03 +0900

    답변기능 추

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -21,12 +21,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AnswerController {
 
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, 
     @RequestParam(value="content") String content){
         Question question = this.questionService.getQuestion(id);
+
         // 답변 저장
+        this.answerService.create(question, content);
         
         return String.format("redirect:/question/detail/%s", id);
     }
