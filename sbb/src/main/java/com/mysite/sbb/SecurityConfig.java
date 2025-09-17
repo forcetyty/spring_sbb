44b0e7b7cbe7adc55c017a6c72a81c0387d0dc01
taeyoung <forcetyty@gmail.com>
2025-09-18 02:24:20 +0900

    사용자 인증 권한 추

--- sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -20,8 +20,8 @@ public class SecurityConfig {
 				-> authorizeHttpRequests.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
 		.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
 		.headers((headers) -> 
-		headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
-		
+		headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+		.formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/"));
 		return http.build();
 		
 	}
